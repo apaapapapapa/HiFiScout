9e8b4f4d2f21e5fde35c4e911f9ca5f3c51c3a3c
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-08-11 09:43:15 +0900

    feat: register FOR MUSIC collector

--- src/crawler/shops/index.js
+++ src/crawler/shops/index.js
@@ -2,5 +2,6 @@ import { audioUnionAdapter } from './audiounion.js';
 import { ippinkanAdapter } from './ippinkan.js';
 import { fujiyaAvicAdapter } from './fujiya-avic.js';
 import { hifidoAdapter } from './hifido.js';
+import { forMusicAdapter } from './formusic.js';
 
-export const SHOP_ADAPTERS = [audioUnionAdapter, ippinkanAdapter, fujiyaAvicAdapter, hifidoAdapter];
+export const SHOP_ADAPTERS = [audioUnionAdapter, ippinkanAdapter, fujiyaAvicAdapter, hifidoAdapter, forMusicAdapter];
