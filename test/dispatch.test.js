9a3b9ac5fa710125ecf6e5a50c971e7a3d986761
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-08-11 17:46:53 +0900

    test: use shared relay variables in dispatch fixtures

--- test/dispatch.test.js
+++ test/dispatch.test.js
@@ -10,8 +10,8 @@ const ONLY_HIFIDO = {
   HIFIDO_ENABLED: 'true',
   HIFIDO_INTERVAL_MINUTES: '30',
   CRAWL_DISPATCH_LEASE_MINUTES: '15',
-  AUDIOUNION_RELAY_URL: 'https://example.lambda-url.ap-northeast-1.on.aws/',
-  AUDIOUNION_RELAY_TOKEN: 'test-relay-token'
+  CRAWL_RELAY_URL: 'https://example.lambda-url.ap-northeast-1.on.aws/',
+  CRAWL_RELAY_TOKEN: 'test-relay-token'
 };
 
 const HIFIDO_AND_AUDIOUNION = {
