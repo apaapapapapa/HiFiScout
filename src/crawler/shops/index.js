8fe1103c30b764bc0b157e0d1bea9549ca2114a8
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-08-12 20:07:49 +0900

    style: format Shimamusen changes with oxfmt defaults

--- src/crawler/shops/index.js
+++ src/crawler/shops/index.js
@@ -1,15 +1,17 @@
-import { normalizeCatalogProducts } from '../../catalog/product-normalizer.js';
-import { audioUnionAdapter } from './audiounion.js';
-import { ippinkanAdapter } from './ippinkan.js';
-import { fujiyaAvicAdapter } from './fujiya-avic.js';
-import { hifidoAdapter } from './hifido.js';
-import { forMusicAdapter } from './formusic.js';
-import { shimamusenAdapter } from './shimamusen.js';
+import { normalizeCatalogProducts } from "../../catalog/product-normalizer.js";
+import { audioUnionAdapter } from "./audiounion.js";
+import { ippinkanAdapter } from "./ippinkan.js";
+import { fujiyaAvicAdapter } from "./fujiya-avic.js";
+import { hifidoAdapter } from "./hifido.js";
+import { forMusicAdapter } from "./formusic.js";
+import { shimamusenAdapter } from "./shimamusen.js";
 // shop-generator:imports
 
 function defineShopPlugin(adapter, definition) {
   if (!adapter?.key || adapter.key !== definition.key) {
-    throw new Error(`shop plugin key mismatch: ${adapter?.key || 'missing'} / ${definition.key}`);
+    throw new Error(
+      `shop plugin key mismatch: ${adapter?.key || "missing"} / ${definition.key}`,
+    );
   }
 
   const plugin = { ...adapter, definition: Object.freeze({ ...definition }) };
@@ -23,37 +25,65 @@ function defineShopPlugin(adapter, definition) {
 
 export const SHOP_PLUGINS = [
   defineShopPlugin(audioUnionAdapter, {
-    key: 'audiounion', name: 'Audio Union', baseUrl: 'https://www.audiounion.jp',
-    intervalEnv: 'AUDIOUNION_INTERVAL_MINUTES', enabledEnv: 'AUDIOUNION_ENABLED',
-    requestDelayEnv: 'AUDIOUNION_REQUEST_DELAY_MS', defaultIntervalMinutes: 30, defaultRequestDelayMs: 10_000
+    key: "audiounion",
+    name: "Audio Union",
+    baseUrl: "https://www.audiounion.jp",
+    intervalEnv: "AUDIOUNION_INTERVAL_MINUTES",
+    enabledEnv: "AUDIOUNION_ENABLED",
+    requestDelayEnv: "AUDIOUNION_REQUEST_DELAY_MS",
+    defaultIntervalMinutes: 30,
+    defaultRequestDelayMs: 10_000,
   }),
   defineShopPlugin(ippinkanAdapter, {
-    key: 'ippinkan', name: '逸品館', baseUrl: 'https://ippinkan.jp',
-    intervalEnv: 'IPPINKAN_INTERVAL_MINUTES', enabledEnv: 'IPPINKAN_ENABLED',
-    requestDelayEnv: 'IPPINKAN_REQUEST_DELAY_MS', defaultIntervalMinutes: 30
+    key: "ippinkan",
+    name: "逸品館",
+    baseUrl: "https://ippinkan.jp",
+    intervalEnv: "IPPINKAN_INTERVAL_MINUTES",
+    enabledEnv: "IPPINKAN_ENABLED",
+    requestDelayEnv: "IPPINKAN_REQUEST_DELAY_MS",
+    defaultIntervalMinutes: 30,
   }),
   defineShopPlugin(fujiyaAvicAdapter, {
-    key: 'fujiya-avic', name: 'フジヤエービック', baseUrl: 'https://www.fujiya-avic.co.jp',
-    intervalEnv: 'FUJIYA_AVIC_INTERVAL_MINUTES', enabledEnv: 'FUJIYA_AVIC_ENABLED',
-    requestDelayEnv: 'FUJIYA_AVIC_REQUEST_DELAY_MS', defaultIntervalMinutes: 30,
-    maxPagesEnv: 'FUJIYA_AVIC_MAX_PAGES', defaultMaxPages: 50
+    key: "fujiya-avic",
+    name: "フジヤエービック",
+    baseUrl: "https://www.fujiya-avic.co.jp",
+    intervalEnv: "FUJIYA_AVIC_INTERVAL_MINUTES",
+    enabledEnv: "FUJIYA_AVIC_ENABLED",
+    requestDelayEnv: "FUJIYA_AVIC_REQUEST_DELAY_MS",
+    defaultIntervalMinutes: 30,
+    maxPagesEnv: "FUJIYA_AVIC_MAX_PAGES",
+    defaultMaxPages: 50,
   }),
   defineShopPlugin(hifidoAdapter, {
-    key: 'hifido', name: 'ハイファイ堂', baseUrl: 'https://www.hifido.co.jp',
-    intervalEnv: 'HIFIDO_INTERVAL_MINUTES', enabledEnv: 'HIFIDO_ENABLED',
-    requestDelayEnv: 'HIFIDO_REQUEST_DELAY_MS', defaultIntervalMinutes: 30,
-    maxPagesEnv: 'HIFIDO_MAX_PAGES', defaultMaxPages: 3
+    key: "hifido",
+    name: "ハイファイ堂",
+    baseUrl: "https://www.hifido.co.jp",
+    intervalEnv: "HIFIDO_INTERVAL_MINUTES",
+    enabledEnv: "HIFIDO_ENABLED",
+    requestDelayEnv: "HIFIDO_REQUEST_DELAY_MS",
+    defaultIntervalMinutes: 30,
+    maxPagesEnv: "HIFIDO_MAX_PAGES",
+    defaultMaxPages: 3,
   }),
   defineShopPlugin(forMusicAdapter, {
-    key: 'formusic', name: 'FOR MUSIC', baseUrl: 'https://shop.formusic.jp',
-    intervalEnv: 'FORMUSIC_INTERVAL_MINUTES', enabledEnv: 'FORMUSIC_ENABLED',
-    requestDelayEnv: 'FORMUSIC_REQUEST_DELAY_MS', defaultIntervalMinutes: 30
+    key: "formusic",
+    name: "FOR MUSIC",
+    baseUrl: "https://shop.formusic.jp",
+    intervalEnv: "FORMUSIC_INTERVAL_MINUTES",
+    enabledEnv: "FORMUSIC_ENABLED",
+    requestDelayEnv: "FORMUSIC_REQUEST_DELAY_MS",
+    defaultIntervalMinutes: 30,
   }),
   defineShopPlugin(shimamusenAdapter, {
-    key: 'shimamusen', name: 'シマムセン', baseUrl: 'https://www.shimamusen.com',
-    intervalEnv: 'SHIMAMUSEN_INTERVAL_MINUTES', enabledEnv: 'SHIMAMUSEN_ENABLED',
-    requestDelayEnv: 'SHIMAMUSEN_REQUEST_DELAY_MS', defaultIntervalMinutes: 60,
-    maxPagesEnv: 'SHIMAMUSEN_MAX_PAGES', defaultMaxPages: 20
+    key: "shimamusen",
+    name: "シマムセン",
+    baseUrl: "https://www.shimamusen.com",
+    intervalEnv: "SHIMAMUSEN_INTERVAL_MINUTES",
+    enabledEnv: "SHIMAMUSEN_ENABLED",
+    requestDelayEnv: "SHIMAMUSEN_REQUEST_DELAY_MS",
+    defaultIntervalMinutes: 60,
+    maxPagesEnv: "SHIMAMUSEN_MAX_PAGES",
+    defaultMaxPages: 20,
   }),
   // shop-generator:plugins
 ];
@@ -62,5 +92,5 @@ export const SHOP_PLUGINS = [
 export const SHOP_ADAPTERS = SHOP_PLUGINS;
 
 export function getShopPlugin(shopKey) {
-  return SHOP_PLUGINS.find(plugin => plugin.key === shopKey) || null;
+  return SHOP_PLUGINS.find((plugin) => plugin.key === shopKey) || null;
 }
