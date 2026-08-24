dbb864a89a34c0d8f95c61ce07b1eb5ae7a6382b
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-08-24 18:57:05 +0900

    test: cover Ippinkan discovered pagination

--- test/ippinkan.test.ts
+++ test/ippinkan.test.ts
@@ -1,6 +1,7 @@
 import { test } from "vite-plus/test";
 import assert from "node:assert/strict";
 import { ippinkanAdapter } from "../src/crawler/shops/ippinkan.js";
+import { discoverPages, initialPageQueue } from "../src/crawler/strategies.js";
 
 const PAGE_URL = "https://ippinkan.jp/shopbrand/U100000/page14/order/";
 const DETAIL_URL = "/shopdetail/000000027581/U100000/page14/order/";
@@ -27,4 +28,22 @@ test("Ippinkan keeps explicit sold-out markers as sold out", () => {
   const [item] = ippinkanAdapter.parse(html, PAGE_URL);
 
   assert.equal(item.stockStatus, "sold_out");
+});
+
+test("Ippinkan follows only pagination links exposed by the storefront", () => {
+  assert.deepEqual(initialPageQueue(ippinkanAdapter, 40), [
+    "https://ippinkan.jp/shopbrand/U100000/",
+  ]);
+
+  const html = `
+    <a href="/shopbrand/U100000/page2/order/">2</a>
+    <a href="https://ippinkan.jp/shopbrand/U100000/page10/order/?ignored=1">10</a>
+    <a href="/shopdetail/000000027581/U100000/page1/order/">product</a>
+    <a href="https://example.com/shopbrand/U100000/page3/order/">external</a>
+  `;
+
+  assert.deepEqual(discoverPages(ippinkanAdapter, html, "https://ippinkan.jp/shopbrand/U100000/"), [
+    "https://ippinkan.jp/shopbrand/U100000/page2/order/",
+    "https://ippinkan.jp/shopbrand/U100000/page10/order/",
+  ]);
 });
